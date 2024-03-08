a2d035e2417bd4f1d03612573eb5e94daab9a418
jspark <jspark@zalesia.com>
2024-03-08 10:31:50 +0900

    MemberSignUp.vue 작성중 / github remote

--- src/pages/member/MemberSignUp.vue
+++ src/pages/member/MemberSignUp.vue
@@ -5,60 +5,60 @@
 <template>
   <div class="default-style-container member-signup-container">
     <div class="member-signup-form">
-      <!-- 첫번째 라인  -->
+      <!-- 이름  -->
       <div>
-        <div class="mr-1">
-          <label for="name">이름</label>
-          <input type="text" id="name" placeholder="이름 입력">
-        </div>
-        <div>
-          <label for="male">남자</label>
-          <input type="radio" id="male" name="gender" value="m">
-          <label for="women">여자</label>
-          <input type="radio" id="women" name="gender" value="w">
-        </div>
+        <label for="name">이름</label>
+        <input type="text" id="name" placeholder="이름 입력">
       </div>
-      <!-- 두번째 라인 -->
+      <!-- 성별 -->
+      <div class="member-signup-gender flex w-full flex-row">
+        <label for="male">남자</label>
+        <input type="radio" id="male" name="gender" value="m">
+        <label for="women">여자</label>
+        <input type="radio" id="women" name="gender" value="w">
+      </div>
+      <!-- 아이디 -->
       <div>
         <label for="id">아이디</label>
         <input type="text" id="id" placeholder="아이디 입력">
         <button class="bg-blue-600">중복체크</button>
       </div>
-      <!-- 세번째 라인 -->
+      <!-- 비밀번호 -->
       <div>
         <label for="password">비밀번호</label>
-        <input type="password" id="password" placeholder="비밀번호를 입력해주세요">
-        <label for="password">재확인</label>
-        <input type="password" id="password" placeholder="다시 입력해주세요">
+        <input type="password" id="password" placeholder="비밀번호 입력">
+        <input type="password" id="password" placeholder="비밀번호 재확인">
+        <button class="bg-blue-600">안전검사</button>
       </div>
-      <!-- 네번째 라인 -->
     </div>
   </div>
 </template>
 
 <style scoped>
 .member-signup-container {
+  height:80%;
 }
 
 .member-signup-form {
   border: 1px solid white;
-}
-
-.member-signup-form label {
-  min-width: 50px;
-}
-
-.member-signup-form input, button {
-  border-radius: 1rem;
-  padding: 0rem 0.5rem;
-}
-
-.member-signup-form button {
-
+  padding:5rem 10rem
 }
 
 .member-signup-form > div {
   display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.member-signup-form > .member-signup-gender{
+  flex-direction: row;
+  justify-content: center;
+}
+.member-signup-form input, button {
+  border-radius: 1rem;
+  padding: 0rem 0.5rem;
+}
+.member-signup-form button {
+
 }
 
 </style>
