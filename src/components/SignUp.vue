26dcd586c8cf34a8b3cbcb65cfbd385d20c2772e
jay sung <namic123@naver.com>
2024-03-11 10:23:28 +0900

    signup 중복확인 요청 및 로직 기본틀 작성

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -28,12 +28,16 @@
       </div>
       <!-- 2번 라인 -->
       <div>
-      <!-- 이메일 -->
+        <!-- 이메일 -->
         <div>
           <label>이메일</label>
           <input type="email" placeholder="이메일 입력" v-model="email">
           <div v-if="isValidateEmail" class="errors">이메일 형식이 아닙니다</div>
-          <button v-bind:disabled="isValidateEmail || email.length <= 0">중복확인</button>
+          <button
+              v-bind:disabled="isValidateEmail || email.length <= 0"
+              @click="checkEmailDuplicates"
+          >중복확인
+          </button>
         </div>
       </div>
       <!-- 3번 라인 -->
@@ -42,8 +46,12 @@
         <div>
           <label>아이디</label>
           <input type="text" placeholder="아이디 입력" v-model="id" @input="updateId($event)">
-          <div v-if="isValidateId " class="errors">{{idErrorMessage}}</div>
-          <button v-bind:disabled="isValidateId">중복확인</button>
+          <div v-if="isValidateId " class="errors">{{ idErrorMessage }}</div>
+          <button
+              v-bind:disabled="isValidateId"
+              @click="checkIdDuplicates"
+          >중복확인
+          </button>
         </div>
         <!-- 비밀번호 -->
         <div>
@@ -51,7 +59,7 @@
           <input type="password" placeholder="비밀번호 입력" v-model="password">
           <input type="password" placeholder="비밀번호 재확인" v-model="passwordCheck">
           <div v-if="comparePasswords" class="errors">비밀번호가 일치하지 않습니다</div>
-          <div v-if="checkPasswordLength" class="errors">{{ password.length === 0 ? "": "8자 이상 입력이 필요합니다"}} </div>
+          <div v-if="checkPasswordLength" class="errors">{{ password.length === 0 ? "" : "8자 이상 입력이 필요합니다" }}</div>
           <div v-if="isValidatePassword" class="errors">8자 이상, 영문자, 숫자, 특수문자를</div>
           <div v-if="isValidatePassword" class="errors">모두 포함해주세요</div>
           <div v-if="isValidatePasswordSuccess" class="success">안전한 비밀번호입니다</div>
@@ -93,40 +101,44 @@ export default {
     });
     // 아이디 형식 검증
     const idPattern = /^(?=.*[a-z])[a-z0-9]+$/;
-    let isValidateId = computed(()=>{
+    let isValidateId = computed(() => {
       return !idPattern.test(id.value) || (id.value.length < 5);
     })
     // 아이디 에러메세지 설정
-    let idErrorMessage = computed(()=>{
-      if(id.value.length === 0){
+    let idErrorMessage = computed(() => {
+      if (id.value.length === 0) {
         return "";
-      }else {
+      } else {
         return "5자 이상 영문 소문자, 숫자만 포함";
       }
     });
     // 비밀번호와 재확인 비교
-    let comparePasswords = computed(()=>{
+    let comparePasswords = computed(() => {
       return password.value !== passwordCheck.value
     });
     //
-    let checkPasswordLength = computed(()=>{
+    let checkPasswordLength = computed(() => {
       return password.value.length < 8;
     })
     let isValidatePassword = ref(false);
     let isValidatePasswordSuccess = ref(false);
-   /*---------------------------- 메소드 영역 ------------------------------------------*/
+
+    /*---------------------------- 메소드 영역 ------------------------------------------*/
 
     function onCloseModal() {
       emit('onCloseModal');
     }
+
     // 한글 입력은 조합형 입력이므로, 키보드에서 키를 누를때마다 바로 입력이 확정되지 않는다.
     // 따라서, v-model이 입력중인 조합형 문자를 바로 반영 못하는 문제가 있으므로, 아래 메서드와 같이 바로 반영
-    function updateNickname(e){
+    function updateNickname(e) {
       nickname.value = e.target.value;
     }
-    function updateId(e){
+
+    function updateId(e) {
       id.value = e.target.value;
     }
+
     // 닉네임 중복 체크
     function checkNicknameDuplicates() {
       console.log(nickname.value);
@@ -142,8 +154,41 @@ export default {
             console.log(error);
           })
     }
+
+    // 닉네임 중복 체크
+    function checkIdDuplicates() {
+      console.log(nickname.value);
+      axios.get('/api/member/check', {
+        params: {
+          id: id.value,
+        }
+      })
+          .then((res) => {
+            console.log(res);
+          })
+          .catch((error) => {
+            console.log(error);
+          })
+    }
+
+    // 닉네임 중복 체크
+    function checkEmailDuplicates() {
+      console.log(nickname.value);
+      axios.get('/api/member/check', {
+        params: {
+          email: email.value,
+        }
+      })
+          .then((res) => {
+            console.log(res);
+          })
+          .catch((error) => {
+            console.log(error);
+          })
+    }
+
     /* 비밀번호 유효성 검증 (영문자, 숫자, 특수문자 포함 여부 확인)*/
-    function handlePasswordType(){
+    function handlePasswordType() {
       // 대,소문자, 숫자 포함 여부 및 특수문자 필수 포함
       const hasUpperCase = /[A-Z]/.test(password.value);
       const hasLowerCase = /[a-z]/.test(password.value);
@@ -156,18 +201,19 @@ export default {
       const findPatternMatches = [hasUpperCase, hasLowerCase, hasNumber];
       const countMatches = findPatternMatches.filter(Boolean).length;
 
-       if(!(countMatches > 1 && hasSpecialChar)){
-         isValidatePassword.value = true;
-       }else {
-         isValidatePasswordSuccess.value = true;
-       }
+      if (!(countMatches > 1 && hasSpecialChar)) {
+        isValidatePassword.value = true;
+      } else {
+        isValidatePasswordSuccess.value = true;
+      }
     }
+
     /* -------------------------------- 상태관리 영역 ---------------------------------*/
     /*------------------------------검증 후, 값 변경 방지 추적 -------------------------*/
-    watch(password,()=>{
+    watch(password, () => {
       isValidatePasswordSuccess.value = false;
     });
-    watch(passwordCheck,()=>{
+    watch(passwordCheck, () => {
       isValidatePasswordSuccess.value = false;
     });
 
@@ -188,7 +234,10 @@ export default {
       isValidatePassword,
       isValidatePasswordSuccess,
       checkNicknameDuplicates,
+      checkIdDuplicates,
+      checkEmailDuplicates,
       handlePasswordType,
+
     }
   }
 }
@@ -266,7 +315,8 @@ button:disabled {
   font-weight: bold;
   color: red;
 }
-.success{
+
+.success {
   font-size: 0.7rem;
   font-weight: bold;
   color: #029a02;
