dd96fee12c2f42420f3311681b74f99c6237d3de
jay sung <namic123@naver.com>
2024-03-12 17:38:24 +0900

    BoardList 목록 출력 및 해당 BoardView로 라우터 이동 추가

---
+++ src/components/BoardList.vue
@@ -0,0 +1,75 @@
+<script setup>
+import axios from "axios";
+import {onMounted, ref} from "vue";
+import {useRouter} from "vue-router";
+
+let boardList = ref(null);
+const router = useRouter();
+onMounted(()=> {
+  axios.get('/api/board/list')
+      .then((res)=>{
+        boardList.value = res.data.boardList;
+      })
+      .catch((error)=>{
+        console.log(error);
+      });
+});
+
+function moveToBoard(boardId){
+  router.push({
+    name:"BoardView",
+    params: {id: boardId},
+  })
+}
+
+</script>
+
+<template>
+  <div class="default-style-container">
+    <div class="board-list">
+      <table>
+        <thead>
+        <tr>
+          <th>번호</th>
+          <th>제목</th>
+          <th>작성자</th>
+          <th>등록일시</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr class="table-body-tr" v-for="board in boardList" :key="board.id"
+            @click="moveToBoard(board.id)">
+          <td>{{board.id}}</td>
+          <td>{{board.title}}</td>
+          <td>{{board.writer}}</td>
+          <td>{{board.inserted}}</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .board-list{
+    width: 100%;
+    border: 1px solid green;
+  }
+  table{
+    width: 100%;
+  }
+  tr{
+    border: 1px solid white;
+  }
+  .table-body-tr:hover{
+    background-color: white;
+    color: black;
+    cursor: pointer;
+  }
+
+  td{
+    border-right: 1px solid white;
+    text-align: center;
+    padding: 0.5rem 0rem;
+  }
+</style>
