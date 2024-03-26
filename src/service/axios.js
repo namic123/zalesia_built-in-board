2920ccfb2328be45d9e707c22fec8957fc1ce27f
jay sung <namic123@naver.com>
2024-03-26 15:35:27 +0900

    axios 설정 변경

--- src/service/axios.js
+++ src/service/axios.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
-export default axios.create({
-    baseURL:'http://localhost:8081'
+const api = axios.create({
+    baseURL:'http://localhost:8081',
 })
+
+
+export default api;
